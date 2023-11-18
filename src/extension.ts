05ca618713ef2bf3c198ec05f347859f74c48671
Chris Redford <credford@gmail.com>
2023-11-18 09:03:53 -0800

    documenting simplification

--- src/extension.ts
+++ src/extension.ts
@@ -43,25 +43,45 @@ export function activate(context: vscode.ExtensionContext) {
 				'SEEK R/N',
 				'SUBI R/N R/N R',
 				'SWIZ R/N R/N R',
-				'TEST EOF',
-				'TEST MRD',
 				'TEST R/N = R/N',
+				'TEST MRD/EOF',
 				'TJMP L',
 				'VOID F',
 			];
+
+			// refer to later comment
+			// type ArgMap = Record<string, string>;
+			// const argMap: ArgMap = {
+			// 	// Found these slower than just pressing space
+			// 	// '=': '|=,<,>|',
+			// 	// 'R': '|X,T,F,M|',
+			// 	'MRD/EOF': '|MRD,EOF|',
+			// };
 
 			for (const line of lines) {
 				const split = line.split(' ');
 				const k = split[0];
 				const c = new vscode.CompletionItem(line);
 				const elements = [k];
-				for (const [i, label] of split.slice(1).entries()) {
-					if (label == "R") {
-						elements.push(`\${${i + 1}|X,T,F,M|}`);
+				if (split.length > 1) {
+					if (split[1] == 'MRD/EOF') {
+						elements.push("${1|MRD,EOF|}");
 					} else {
-						elements.push(`\${${i + 1}:${label}}`);
+						// Just add space to end so command can be followed immediately by argument
+						elements.push('');
 					}
 				}
+
+				// This is a nice idea but all the tab pressing is worse than just pressing space
+				// It would work if the default completion could be changed to space just for this extension
+				// for (const [i, label] of split.slice(1).entries()) {
+				// 	let annotation = `:${label}`;
+				// 	if (label in argMap) {
+				// 		annotation = argMap[label];
+				// 	}
+				// 	elements.push(`\${${i + 1}${annotation}}`);
+				// }
+
 				c.insertText = new vscode.SnippetString(elements.join(' '));
 				completions.push(c);
 			}
